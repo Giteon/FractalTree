73ff6320c52a205924a0d478e793800fc288b4a0
giteon <jugglerfox@gmail.com>
2015-02-10 10:42:06 -0800

    la

--- build-tmp/source/FractalTree.java
+++ build-tmp/source/FractalTree.java
@@ -1,6 +1,22 @@
-private double fractionLength = .8; 
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class FractalTree extends PApplet {
+
+private double fractionLength = .8f; 
 private int smallestBranch = 20; 
-private double branchAngle = .6;  
+private double branchAngle = .6f;  
 public void setup() 
 {   
 	size(640,480);    
@@ -14,7 +30,7 @@ public void draw()
 	drawBranches(320,380,100,3*Math.PI/2,255);  //will add later 
 
 } 
-public void drawBranches(int x,int y, double branchLength, double angle, color col) 
+public void drawBranches(int x,int y, double branchLength, double angle, int col) 
 {   
 	
 double angle1 = angle + branchAngle;
@@ -47,42 +63,42 @@ if (branchLength > smallestBranch)
  /*TBD*/int call4 = color((int)(Math.random()*(int)(y/10)+27),(int)(Math.random()*(int)(y/10)+149),(int)(Math.random()*(int)(y/10)+186));
 
  /*Kraken Yellow*/int call5 = color((int)(Math.random()*(int)(y/10)+240),(int)(Math.random()*(int)(y/10)+177),(int)(Math.random()*(int)(y/10)+23));
-if (Math.random()>.9)
+if (Math.random()>.9f)
 {
 	drawBranches(endX1,endY1,branchLength,angle1,call1);
 	drawBranches(endX2,endY2,branchLength,angle2,call2);
 }
-else if (Math.random()>.78)
+else if (Math.random()>.78f)
 {
 	drawBranches(endX1,endY1,branchLength,angle1,call1);
 	drawBranches(endX2,endY2,branchLength,angle2,call3);
 }
-else if (Math.random()>.66)
+else if (Math.random()>.66f)
 {
 	drawBranches(endX1,endY1,branchLength,angle1,call3);
 	drawBranches(endX2,endY2,branchLength,angle2,call2);
 }
-else if (Math.random()>.54)
+else if (Math.random()>.54f)
 {
 	drawBranches(endX1,endY1,branchLength,angle1,call4);
 	drawBranches(endX2,endY2,branchLength,angle2,call2);
 }
-else if (Math.random()>.42)
+else if (Math.random()>.42f)
 {
 	drawBranches(endX1,endY1,branchLength,angle1,call1);
 	drawBranches(endX2,endY2,branchLength,angle2,call4);
 }
-else if(Math.random()>.3)
+else if(Math.random()>.3f)
 {
 	drawBranches(endX1,endY1,branchLength,angle1,call5);
 	drawBranches(endX2,endY2,branchLength,angle2,call1);
 }
-else if(Math.random()>.18)
+else if(Math.random()>.18f)
 {
 	drawBranches(endX1,endY1,branchLength,angle1,call2);
 	drawBranches(endX2,endY2,branchLength,angle2,call5);
 }
-else if(Math.random()>.06)
+else if(Math.random()>.06f)
 {
 	drawBranches(endX1,endY1,branchLength,angle1,call5);
 	drawBranches(endX2,endY2,branchLength,angle2,call3);
@@ -119,4 +135,13 @@ public void keyPressed() {
 	}
 	}
 	redraw();
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "FractalTree" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
 }
